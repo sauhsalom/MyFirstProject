ef650990bf69acda23326118a505434222b5672c
sauh <sauhgonsalves@hotmail.com>
2024-04-08 11:43:02 -0300

    meu primeiro commit

---
+++ MyFirstProjectReadFile/MyFirstProjectReadFile/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyFirstProjectReadFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = "C:\\Users\\sauh\\Downloads\\Com.csv";
+            var conteudo = File.ReadAllText(FilePath);
+
+            string[] splitFile = conteudo.Split('\n');
+
+            foreach (var row in splitFile)
+            {
+                Console.Write("Deseja modificar a linha? ");
+                Console.WriteLine(row);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
